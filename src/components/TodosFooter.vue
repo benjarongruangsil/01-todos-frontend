f25d29e9ee082f46132f417cda822145c19076a4
benjarongruangsil <5806021620082@fitm.kmutnb.ac.th>
2018-01-25 16:34:12 +0700

    4

--- src/components/TodosFooter.vue
+++ src/components/TodosFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <p class="is-pulled-left" >{{all}} items left</p></br>
-    <p class="is-pulled-left" >{{active}} items left</p></br>
-    <p class="is-pulled-left" >{{Completed}} items left</p></br>
-    
+    <p class="is-pulled-left"  v-if="visibility === 'all'">{{count}} items left</p>
+    <p class="is-pulled-left"  v-if="visibility === 'active'">{{count}} items left</p>
+    <p class="is-pulled-left"  v-if="visibility === 'completed'" >{{count}} items left</p>
+
     <visibility-input/>
   </div>
 </template>
@@ -22,20 +22,17 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['todos']),
-    all () {
-      if (this.todos) {
-        return this.todos.length
-      }
-    },
-    active () {
-      if (this.todos) {
+    ...mapGetters([
+      'todos',
+      'visibility'
+    ]),
+    count () {
+      if (this.visibility === 'active') {
         return this.todos.filter(todo => todo.completed === false).length
-      }
-    },
-    Completed () {
-      if (this.todos) {
+      } else if (this.visibility === 'completed') {
         return this.todos.filter(todo => todo.completed === true).length
+      } else if (this.visibility === 'all') {
+        return this.todos.length
       }
     }
   }
